1811cde6f822173d5ffe650040bc8d29bc5115ef
B23 <B23@c.scng.si>
2018-04-11 11:25:23 +0200

    Začetni commit

---
+++ Druga/Druga/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Druga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stari Donald farmo ima");
+            Console.WriteLine("IAIAO");
+            Console.WriteLine("Na tej farmi osla dva");
+            Console.WriteLine("IAIAO");
+        }
+    }
+}
